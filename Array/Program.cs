e7f4d3b1378c01fd258ade5db1a580a2b2ca31f2
LSLIM <LSLIM@LS>
2018-03-11 09:30:55 +0100

    I add a for loop int the program

--- Array/Program.cs
+++ Array/Program.cs
@@ -10,6 +10,15 @@ namespace Array
     {
         static void Main(string[] args)
         {
+            string[] names = { "Tom", "Judith", "Max", "Marlon", "Mike" };
+            Console.WriteLine($"{names[1]} is second and {names[4]} is fourth");
+            names[1] = "Jolin";
+            names[2] = "Helen";
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
         }
     }
 }
